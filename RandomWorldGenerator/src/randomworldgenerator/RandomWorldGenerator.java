a09c53d73496b037e88e33485c0c56a2f911674f
angie0904 <168436184+angie0904@users.noreply.github.com>
2024-05-21 11:37:57 +0200

    kjghjgfh

--- RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
+++ RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
@@ -15,7 +15,7 @@ public class RandomWorldGenerator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("holaa");
     }
     
 }
